f49e396fa32fe8fff7cbe7e955d49318450f62e2
Tomas <tomasjuocepis@gmail.com>
2014-03-06 17:11:59 -0600

    Adding RXTX libraries

--- SnuffCommandCenter/src/ieee/uic/robotics/SnuffCommandCenter.java
+++ SnuffCommandCenter/src/ieee/uic/robotics/SnuffCommandCenter.java
@@ -1,10 +1,27 @@
 package ieee.uic.robotics;
 
-public class SnuffCommandCenter {
+import java.util.Enumeration;
+
+import gnu.io.*;
+
+public class SnuffCommandCenter implements SerialPortEventListener {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated hello again tom
-
+		
+		System.out.println("YEAYAH!!");
+		listAllPorts();
 	}
 
+	@Override
+	public void serialEvent(SerialPortEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void listAllPorts() {
+		Enumeration ports = CommPortIdentifier.getPortIdentifiers();  
+	    
+	    while(ports.hasMoreElements())  
+	        System.out.println(((CommPortIdentifier)ports.nextElement()).getName());  
+	}    
 }
